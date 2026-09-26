4fbe9ea856c8d6a671ebc4d2f1883e8aad575739
AVK-2619 <abdullah262009@gmail.com>
2026-09-26 18:59:54 +0530

    Edit

--- JS/LoginPage.js
+++ JS/LoginPage.js
@@ -25,7 +25,7 @@ async function Login(){
 }
 
 function SignUp(){
-    window.location.href = "../HTML/index.html"
+    window.location.href = "../index.html"
 }
 
 window.addEventListener("keydown", (event) => {

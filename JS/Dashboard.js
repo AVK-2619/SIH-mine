9cc9741ec1ad48d711181f9c0e91d89a732936b3
AVK-2619 <abdullah262009@gmail.com>
2026-09-27 01:12:49 +0530

    Skills

--- JS/Dashboard.js
+++ JS/Dashboard.js
@@ -1,10 +1,11 @@
+const ResponseData = JSON.parse(localStorage.getItem("Data"));
+
 function SignOut(){
     localStorage.removeItem("Data");
     window.location.href = "../HTML/LoginPage.html";
 }
 
 function Loadd(){
-    const ResponseData = JSON.parse(localStorage.getItem("Data"));
     document.getElementById("Username").textContent += ResponseData[0].Username;
     document.getElementById("CompanyName").textContent += ResponseData[0].CompanyName;
     document.getElementById("Age").textContent += ResponseData[0].Age;
@@ -19,4 +20,24 @@ function Loadd(){
     }
 }
 
+function AddSkill(SkillName, SkillLevel){
+    document.getElementById("Content").innerHTML += `
+    <button type="button", class="Skills">
+        <p id="SkillName">Skill name: ${SkillName}</p>
+        <label for="SkillProgress">Level: ${SkillLevel}</label><br>
+        <progress id="SkillProgress", value="${SkillLevel}", max="100"></progress>
+    </button>`;
+}
+
+async function RenderSkills(UserID){
+    const response = await fetch(`http://localhost:3000/GetSkills/${UserID}`);
+    const ResponseData = await response.json();
+    AddSkill("Java", ResponseData['Java']);
+    AddSkill("Python", ResponseData['Python']);
+    AddSkill("C", ResponseData['C']);
+    AddSkill("HTML", ResponseData['Html']);
+    AddSkill("CSS", ResponseData['Css']);
+}
+
+window.addEventListener("DOMContentLoaded", RenderSkills(ResponseData[0]['UserID']));
 window.addEventListener("DOMContentLoaded", Loadd());
